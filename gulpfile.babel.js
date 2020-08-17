5c564b044724d099c142ff54c2b8b68f340a1abc
Jack Lowrie <jack@alphaparticle.com>
2020-08-17 08:21:30 -0400

    add basic lint & watch tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,26 @@
 'use strict';
 
 const gulp = require('gulp');
+const eslint = require('gulp-eslint');
 
 function hello(done) {
-	console.log("Hi, I'm Gizmo!");
+	console.log("Hi, I'm Gizmo!")
 	return done();
 }
 
+function lint() {
+	return gulp
+		.src('./gulpfile.babel.js')
+		.pipe(eslint())
+		.pipe(eslint.format());
+}
+
+function watchGulpFile() {
+	return gulp
+		.watch('./gulpfile.babel.js', lint);
+}
+
 exports.hello = hello;
+exports.lint = lint;
+exports.watch = watchGulpFile;
+exports.default = gulp.series(lint, watchGulpFile);
