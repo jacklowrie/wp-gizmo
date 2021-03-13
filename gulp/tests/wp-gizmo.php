23a8de83f2a71a068221d704c886ed8514cf68f8
Jack Lowrie <jack@alphaparticle.com>
2021-03-13 14:16:36 -0500

    npm install

---
+++ gulp/tests/wp-gizmo.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * WP-Gizmo
+ *
+ * @package           wp-gizmo
+ * @author            Jack Lowrie
+ * @license           GPL-3.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP-Gizmo
+ * Plugin URI:        github.com/jacklowrie/wp-gizmo
+ * Description:       A starter plugin and build process for making WordPress plugins
+ * Version:           1.0.0
+ * Requires at least: 5.0
+ * Requires PHP:      7.0
+ * Author:            Gizmo Author
+ * Author URI:        https://wpgizmo.dev
+ * Text Domain:       plugin-slug
+ * License:           GPL v3 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+declare( strict_types = 1 );
