f8109535c5620c4ab6b3de90c7c9b03edf7b08dd
Jack Lowrie <jack@alphaparticle.com>
2021-03-13 10:51:36 -0500

    refactor bundle to use lazypipe

--- gulp/pipes.js
+++ gulp/pipes.js
@@ -3,10 +3,40 @@
 const lazypipe = require('lazypipe');
 
 const eslint = require('gulp-eslint');
+const config = require('../config.json');
+const rename = require('gulp-rename');
+const stringReplace = require('gulp-string-replace');
+
+
+const stringReplaceOptions = {
+    logs: {
+      enabled: false,
+    },
+    searchValue: 'regex',
+  };
 
 
 const lint = lazypipe()
   .pipe(eslint)
   .pipe(eslint.format);
 
+const bundle = lazypipe()
+  .pipe(
+    rename,
+    `${config.plugin.slug}.php`
+  )
+  .pipe(
+    stringReplace,
+    /wp-gizmo/g,
+    config.plugin.slug,
+    stringReplaceOptions
+  )
+  .pipe(
+    stringReplace,
+    /WP-Gizmo/g,
+    config.plugin.name,
+    stringReplaceOptions
+  )
+
 exports.lint = lint;
+exports.bundle = bundle;
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,8 +3,6 @@
 const pipes = require('./gulp/pipes');
 
 const gulp = require('gulp');
-const rename = require('gulp-rename');
-const stringReplace = require('gulp-string-replace');
 const config = require('./config.json');
 const fs = require('fs');
 const rimraf = require('rimraf');
@@ -18,50 +16,33 @@ function hello(done) {
   return done();
 }
 
-function lint() {
-  return gulp
-      .src('./gulpfile.babel.js')
-      .pipe(pipes.lint());
+function lint(done) {
+  gulp.src('./gulpfile.babel.js')
+    .pipe(pipes.lint());
+  return done();
 }
 
-function watchGulpFile() {
-  return gulp
-      .watch('./gulpfile.babel.js', lint);
+function watchGulpFile(done) {
+  gulp.watch('./gulpfile.babel.js', lint);
+  done();
 }
 
-// function bundle() {
-//   if (fs.existsSync(productionDirectory)) {
-//     console.log('removing old production directory...');
-//     rimraf.sync( productionDirectory );
-//   }
-//   fs.mkdirSync(productionDirectory);
+function bundle(done) {
+  if (fs.existsSync(productionDirectory)) {
+    console.log('removing old production directory...');
+    rimraf.sync( productionDirectory );
+  }
+  fs.mkdirSync(productionDirectory);
 
-//   const stringReplaceOptions = {
-//     logs: {
-//       enabled: false,
-//     },
-//     searchValue: 'regex',
-//   };
+  gulp.src('wp-gizmo.php')
+    .pipe(pipes.bundle())
+    .pipe(gulp.dest(productionDirectory));
 
-//   return gulp
-//       .src('wp-gizmo.php')
-//       .pipe(rename(`${config.plugin.slug}.php`))
-//       .pipe(stringReplace(
-//           /wp-gizmo/g,
-//           config.plugin.slug,
-//           stringReplaceOptions
-//       ))
-//       .pipe(stringReplace(
-//           /WP-Gizmo/g,
-//           config.plugin.name,
-//           stringReplaceOptions
-//       ))
-
-//       .pipe(gulp.dest(productionDirectory));
-// }
+  return done();
+}
 
 exports.hello = hello;
 exports.lint = lint;
 exports.watch = watchGulpFile;
 // exports.default = gulp.series(lint, watchGulpFile);
-// exports.bundle = bundle;
+exports.bundle = bundle;
