391cd5d8e4980d25d445de64037cc7bf678d03df
Jack Lowrie <jack@alphaparticle.com>
2021-05-30 22:32:36 -0500

    add plugin unit test scaffolding

---
+++ tests/unit/test-gizmo.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Class GizmoTest
+ *
+ * @package wp-gizmo
+ */
+declare( strict_types = 1 );
+
+use WP_Gizmo\Gizmo\Gizmo;
+
+/**
+ * Sample test case.
+ */
+class UnitTest extends WP_UnitTestCase {
+
+	public function test_sample() {
+		$this->assertTrue( true );
+	}
+}
