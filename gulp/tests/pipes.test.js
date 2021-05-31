c32433adf59c6e0e63d41e756baa64efc6406408
Jack Lowrie <jack@alphaparticle.com>
2021-05-30 23:20:23 -0500

    update gulp pipeline tests

--- gulp/tests/pipes.test.js
+++ gulp/tests/pipes.test.js
@@ -31,13 +31,95 @@ describe('pipes.js', () => {
         .pipe(vfsFake.dest(jsFile.dest));
   });
 
-  describe('bundle pipe', () => {
-    test('renames main plugin file', () => {
+  describe('nameReplace pipe', () => {
+    test('replaces WP-Gizmo with the plugin name', () => {
       const testGizmo = {
         src: [
           {
             path: 'wp-gizmo.php',
-            contents: new Buffer('not a real file.'),
+            contents: new Buffer('WP-Gizmo is the plugin name.'),
+          },
+        ],
+        dest: (files) => {
+          const testFile = files[0];
+
+          expect(testFile.contents.toString('utf-8'))
+              .toBe('New Plugin is the plugin name.');
+        },
+      };
+
+      vfsFake.src(testGizmo.src)
+          .pipe(pipes.nameReplace())
+          .pipe(vfsFake.dest(testGizmo.dest));
+    });
+    test('replaces WP_Gizmo with the plugin name underscored', () => {
+      const testGizmo = {
+        src: [
+          {
+            path: 'wp-gizmo.php',
+            contents: new Buffer('WP_Gizmo is the namespace.'),
+          },
+        ],
+        dest: (files) => {
+          const testFile = files[0];
+
+          expect(testFile.contents.toString('utf-8'))
+              .toBe('New_Plugin is the namespace.');
+        },
+      };
+
+      vfsFake.src(testGizmo.src)
+          .pipe(pipes.nameReplace())
+          .pipe(vfsFake.dest(testGizmo.dest));
+    });
+    test('Replaces Gizmo with PascalCase plugin name', () => {
+      const testGizmo = {
+        src: [
+          {
+            path: 'wp-gizmo.php',
+            contents: new Buffer('Gizmo definition.'),
+          },
+        ],
+        dest: (files) => {
+          const testFile = files[0];
+
+          expect(testFile.contents.toString('utf-8'))
+              .toBe('NewPlugin definition.');
+        },
+      };
+
+      vfsFake.src(testGizmo.src)
+          .pipe(pipes.nameReplace())
+          .pipe(vfsFake.dest(testGizmo.dest));
+    });
+    test('Replaces gizmo with snake_case plugin slug', () => {
+      const testGizmo = {
+        src: [
+          {
+            path: 'wp-gizmo.php',
+            contents: new Buffer('$gizmo variable.'),
+          },
+        ],
+        dest: (files) => {
+          const testFile = files[0];
+
+          expect(testFile.contents.toString('utf-8'))
+              .toBe('$new_plugin variable.');
+        },
+      };
+
+      vfsFake.src(testGizmo.src)
+          .pipe(pipes.nameReplace())
+          .pipe(vfsFake.dest(testGizmo.dest));
+    });
+  });
+  describe('renameSlug pipe', () => {
+    test('renames plugin file to slug', () => {
+      const testGizmo = {
+        src: [
+          {
+            path: 'wp-gizmo.php',
+            contents: new Buffer('wp-gizmo is the slug.'),
           },
         ],
         dest: (files) => {
@@ -50,50 +132,31 @@ describe('pipes.js', () => {
       };
 
       vfsFake.src(testGizmo.src)
-          .pipe(pipes.bundle())
+          .pipe(pipes.renameSlug())
           .pipe(vfsFake.dest(testGizmo.dest));
     });
   });
+  describe('renameMainPluginClass pipe', () => {
+    test('renames main plugin file to PascalCase plugin name', () => {
+      const testGizmo = {
+        src: [
+          {
+            path: 'gizmo.php',
+            contents: new Buffer('wp-gizmo is the slug.'),
+          },
+        ],
+        dest: (files) => {
+          const testFile = files[0];
+          testFile.basename = path.basename(testFile.path);
 
-  test('replaces plugin slug', () => {
-    const testGizmo = {
-      src: [
-        {
-          path: 'wp-gizmo.php',
-          contents: new Buffer('not a real file. wp-gizmo is the plugin slug.'),
+          expect(testFile.basename)
+              .toBe('NewPlugin.php');
         },
-      ],
-      dest: (files) => {
-        const testFile = files[0];
+      };
 
-        expect(testFile.contents.toString('utf-8'))
-            .toBe('not a real file. new-plugin is the plugin slug.');
-      },
-    };
-
-    vfsFake.src(testGizmo.src)
-        .pipe(pipes.bundle())
-        .pipe(vfsFake.dest(testGizmo.dest));
-  });
-
-  test('replaces plugin slug', () => {
-    const testGizmo = {
-      src: [
-        {
-          path: 'wp-gizmo.php',
-          contents: new Buffer('not a real file. WP-Gizmo is the plugin name.')
-        },
-      ],
-      dest: (files) => {
-        const testFile = files[0];
-
-        expect(testFile.contents.toString('utf-8'))
-            .toBe('not a real file. New Plugin is the plugin name.');
-      },
-    };
-
-    vfsFake.src(testGizmo.src)
-        .pipe(pipes.bundle())
-        .pipe(vfsFake.dest(testGizmo.dest));
+      vfsFake.src(testGizmo.src)
+          .pipe(pipes.renameMainPluginClass())
+          .pipe(vfsFake.dest(testGizmo.dest));
+    });
   });
 });
